7aee200e6060d2a17ae86df26e16a8ea6bf82f8a
Noah Mendoza <mendoza.841@buckeyemail.osu.edu>
2024-02-24 13:38:07 -0500

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./Home/Home";
 import Products from "./Products/Products";
+import Orders from './Orders/Orders'
+import OrderDetail from './Orders/OrderDetail';
 
 function App() {
   const openMenu = () => {
@@ -28,6 +30,7 @@ function App() {
                   <div className="header-links">
                       <Link to="/">Home</Link>
                       <Link to="/catalog">Catalog</Link>
+                      <Link to="/orders">Orders</Link>
                   </div>
               </header>
               <aside className="sidebar">
@@ -47,6 +50,8 @@ function App() {
                     <Routes>
                       <Route path="/" element={<Home />} />
                       <Route path="/catalog" element={<Products />} />
+                      <Route path="orders" element={<Orders />}></Route>
+                      <Route path="/order/:id" element={<OrderDetail />}></Route>
                     </Routes>
                 </main>
                 <footer className="footer">
